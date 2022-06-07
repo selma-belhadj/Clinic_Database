775f93b99b09787ad1ff86ba777415481d049a07
selma <belhadjselma1@gmail.com>
2022-06-07 10:35:16 +0100

    create patients, invoices & treatments tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,21 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE patients (
+    id INT not null primary key,
+    name varchar(100) not null,          
+    date_of_birth  date not null           
+);
+
+CREATE TABLE invoices (
+    id INT not null primary key,
+    total_amount decimal not null,
+    generated_at timestamp not null,
+    payed_at timestamp not null,
+    medical_history_id int not null
+);
+
+CREATE TABLE treatments (
+    id int not null primary key,
+    type varchar(100) not null, 
+    name varchar(100) not null          
 );
