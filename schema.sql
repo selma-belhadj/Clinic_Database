6eea546f69a4052611bd251d68524ae319ff79f0
Solomon Achilles <solohag92@gmail.com>
2022-06-07 09:53:43 +0000

    Create tables for medical_hist and invoice_items

--- schema.sql
+++ schema.sql
@@ -2,20 +2,40 @@
 
 CREATE TABLE patients (
     id INT not null primary key,
-    name varchar(100) not null,          
-    date_of_birth  date not null           
+    name varchar(100),          
+    date_of_birth  date            
 );
 
 CREATE TABLE invoices (
     id INT not null primary key,
-    total_amount decimal not null,
-    generated_at timestamp not null,
-    payed_at timestamp not null,
-    medical_history_id int not null
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id int,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
 );
 
 CREATE TABLE treatments (
     id int not null primary key,
-    type varchar(100) not null, 
-    name varchar(100) not null          
+    type varchar(100), 
+    name varchar(100)           
 );
+
+CREATE TABLE medical_histories (
+    id INT not null primary key,
+    admitted_at timestamp,
+    patient_id INT,
+    status varchar(100),
+    FOREIGN KEY (patient_id) REFERENCES patients (id)
+)
+
+CREATE TABLE invoice_items (
+    id INT not null primary key,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+    FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+)
